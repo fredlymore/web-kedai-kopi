1875d19088ce89cb44c312051f65a2e43f558bb7
fredlymore <64302502+fredlymore@users.noreply.github.com>
2023-05-28 20:52:14 +0700

    membuat website kopi kenangan

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// toggle class active
+const navbarNav = document.querySelector
+('.navbar-nav');
+// Ketika Hamburger menu di klik
+document.querySelector('#hamburger-menu').
+onclick = () => {
+ navbarNav.classList.toggle('active');
+
+};
+
+// klik di luar sidebar untuk menghilang nav
+const hamburger = document.querySelector('#hamburger-menu');
+
+document.addEventListener('click', function(e) {
+    if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove('active');
+    }
+})
